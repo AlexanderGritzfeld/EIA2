7550ed66cc3d12c6d58b1ea28b3939f4024a2af5
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-03-24 21:51:59 +0100

    Create typecript9.ts
    Arrays stehen, test logout

---
+++ L01/typescript/typecript9.ts
@@ -0,0 +1,14 @@
+
+namespace L01 { //damit es nicht rummeckert, dass ich Namen schon vergeben habe
+
+let part1: string[] = ["Das Leben ", "Unser Nachbar ", "Ein Pinguin ", "Anarchie ", "Die Welt, so wie wir sie kennen, "];
+
+let part2: string[] = ["befreit ", "konsumiert ", "tötet ", "legalisiert ", "projeziert "];
+
+let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "'Sein'"]
+
+console.log(part1);
+console.log(part2);
+console.log(part3);
+
+}
