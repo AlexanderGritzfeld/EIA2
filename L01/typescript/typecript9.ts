0c55fd293704b0a9d64c58223ac270a07500d5e9
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-03-25 13:18:34 +0100

    Update typecript9.ts
    soweit fertig (siehe Kommentare)

--- L01/typescript/typecript9.ts
+++ L01/typescript/typecript9.ts
@@ -1,14 +1,31 @@
 
 namespace L01 { //damit es nicht rummeckert, dass ich Namen schon vergeben habe
 
-let part1: string[] = ["Das Leben ", "Unser Nachbar ", "Ein Pinguin ", "Anarchie ", "Die Welt, so wie wir sie kennen, "];
+//Arrays in denen die Gedichtsfetzen liegen
 
-let part2: string[] = ["befreit ", "konsumiert ", "tötet ", "legalisiert ", "projeziert "];
+let part1: string[] = ["Das Leben", "Unser Nachbar", "Ein Pinguin", "Anarchie", "Die Welt, so wie wir sie kennen,"];
 
-let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "'Sein'"];
+let part2: string[] = ["befreit", "konsumiert", "tötet", "legalisiert", "prophezeit"];
 
-console.log(part1);
-console.log(part2);
-console.log(part3);
+let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "uns"];
 
+
+
+//jetzt sollen die Arrays geshuffelt werden
+
+part1.sort((a, b) => 0.5 - Math.random());
+part2.sort((a, b) => 0.5 - Math.random());
+part3.sort((a, b) => 0.5 - Math.random());
+
+
+//jetzt sollen die Fetzen zusammengeführt werden und in der Konsole ausgegeben werden
+
+let x: number = 0;
+
+while (x <= part1.length) {
+    console.log(part1[x], part2[x], part3[x]);
+    x++;
 }
+
+
+} //Ende Namespace
