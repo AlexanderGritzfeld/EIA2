15e86205c7356507406a94e1b803b0eb4f4faf12
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-03-25 08:23:32 +0100

    Update typecript9.ts

--- L01/typescript/typecript9.ts
+++ L01/typescript/typecript9.ts
@@ -5,7 +5,7 @@ let part1: string[] = ["Das Leben ", "Unser Nachbar ", "Ein Pinguin ", "Anarchie
 
 let part2: string[] = ["befreit ", "konsumiert ", "tötet ", "legalisiert ", "projeziert "];
 
-let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "'Sein'"]
+let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "'Sein'"];
 
 console.log(part1);
 console.log(part2);
