5a5ed66e7e3a6297204c1998b1fda1d25307ed00
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-03-25 08:23:18 +0100

    Create typecript9.js

---
+++ L01/typescript/typecript9.js
@@ -0,0 +1,10 @@
+var L01;
+(function (L01) {
+    var part1 = ["Das Leben ", "Unser Nachbar ", "Ein Pinguin ", "Anarchie ", "Die Welt, so wie wir sie kennen, "];
+    var part2 = ["befreit ", "konsumiert ", "tötet ", "legalisiert ", "projeziert "];
+    var part3 = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "'Sein'"];
+    console.log(part1);
+    console.log(part2);
+    console.log(part3);
+})(L01 || (L01 = {}));
+//# sourceMappingURL=typecript9.js.map
