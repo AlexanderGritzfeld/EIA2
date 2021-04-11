544cc046f9ba324ea743ba62f6aa2625e7e496c2
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-04-11 13:41:43 +0200

    Update typecript9.js

--- L01/typescript/typecript9.js
+++ L01/typescript/typecript9.js
@@ -1,15 +1,16 @@
+"use strict";
 var L01;
 (function (L01) {
     //Arrays in denen die Gedichtsfetzen liegen
-    var part1 = ["Das Leben", "Unser Nachbar", "Ein Pinguin", "Anarchie", "Die Welt, so wie wir sie kennen,"];
-    var part2 = ["befreit", "konsumiert", "tötet", "legalisiert", "prophezeit"];
-    var part3 = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "uns"];
+    let part1 = ["Das Leben", "Unser Nachbar", "Ein Pinguin", "Anarchie", "Die Welt, so wie wir sie kennen,"];
+    let part2 = ["befreit", "konsumiert", "tötet", "legalisiert", "prophezeit"];
+    let part3 = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "uns"];
     //jetzt sollen die Arrays geshuffelt werden
-    part1.sort(function (a, b) { return 0.5 - Math.random(); });
-    part2.sort(function (a, b) { return 0.5 - Math.random(); });
-    part3.sort(function (a, b) { return 0.5 - Math.random(); });
+    part1.sort((a, b) => 0.5 - Math.random());
+    part2.sort((a, b) => 0.5 - Math.random());
+    part3.sort((a, b) => 0.5 - Math.random());
     //jetzt sollen die Fetzen zusammengeführt werden und in der Konsole ausgegeben werden
-    var x = 0;
+    let x = 0;
     while (x < part1.length) {
         console.log(part1[x], part2[x], part3[x]);
         x++;
