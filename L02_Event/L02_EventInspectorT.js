d0affa86bb3e31a7768cae79dc0f1b3e3a78f761
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-04-17 10:57:35 +0200

    Create L02_EventInspectorT.js

---
+++ L02_Event/L02_EventInspectorT.js
@@ -0,0 +1,9 @@
+"use strict";
+var L02E;
+(function (L02E) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        console.log("Test");
+    }
+})(L02E || (L02E = {})); //Ende namespace
+//# sourceMappingURL=L02_EventInspectorT.js.map
