6b6fa8e1362679f688015e94cec82284b360336c
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-04-17 14:24:29 +0200

    Update L02_EventInspectorT.ts

--- L02_Event/L02_EventInspectorT.ts
+++ L02_Event/L02_EventInspectorT.ts
@@ -4,7 +4,42 @@ window.addEventListener("load", handleLoad);
 
 
 function handleLoad(): void {
-    console.log("Test");
+    
+    document.addEventListener ("mousemove", setInfoBox);
+
+    document.addEventListener ("click", logInfo);
+    document.addEventListener ("keyup", logInfo);
+
+    document.body.addEventListener ("click", logInfo);
+    document.body.addEventListener ("keyup", logInfo);
+
+    let documentDivs: NodeListOf<HTMLDivElement> = document.querySelectorAll("div");
+
+    for (let i: number = 0; i < documentDivs.length; i++) {
+        documentDivs[i].addEventListener ("click", logInfo);
+        documentDivs[i].addEventListener ("keyup", logInfo);
+    }
+}
+
+function setInfoBox(_event: MouseEvent): void {
+    let spanElement: HTMLSpanElement = <HTMLSpanElement>document.querySelector("#span");
+
+    let x: number = _event.clientX;
+    let y: number = _event.clientY;
+    let mousePosition: string = "X-Coordinates: " + x + "," + "<br>" + "Y-Coordinates: " + y + ",";
+    let eventTarget: EventTarget = <EventTarget>_event.target;
+
+    spanElement.innerHTML = mousePosition + "<br>" + eventTarget;
+    spanElement.style.top = y + 8 + "px";
+    spanElement.style.left = x + 10 + "px";
+}
+
+function logInfo(_event: Event): void {
+
+    console.log(_event.type);
+    console.log(_event.target);
+    console.log(_event.currentTarget);
+    console.log(_event.composedPath());
 }
 
 } //Ende namespace
