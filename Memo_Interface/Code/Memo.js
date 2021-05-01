f057572c76519d81a3f43742a30abb53e09989dc
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-01 22:31:54 +0200

    Update Memo.js

--- Memo_Interface/Code/Memo.js
+++ Memo_Interface/Code/Memo.js
@@ -12,6 +12,7 @@ var MemorySettings;
         let startMemory = document.querySelector(".start_bttn");
         startMemory.addEventListener("click", main);
     }
+    let body;
     let formData;
     let size;
     let bgColor;
@@ -22,14 +23,14 @@ var MemorySettings;
         let card = document.createElement("div");
         card.innerHTML = "<p>" + _cardContent + "</p>";
         card.classList.add("card");
-        card.classList.add("is_hidden");
+        card.classList.add("hidden");
         cardArray.push(card);
         checkOthers.push(card);
         card.addEventListener("click", clickHandler);
         card.style.width = size + "px";
         card.style.height = size + "px";
         if (bgColor) {
-            card.style.backgroundColor = bgColor.toString();
+            body.style.background = bgColor.toString();
         }
         if (fontColor) {
             card.style.color = fontColor.toString();
@@ -37,14 +38,17 @@ var MemorySettings;
         if (fontStyle) {
             card.style.fontFamily = fontStyle.toString();
         }
+        if (cardColor) {
+            card.style.backgroundColor = cardColor.toString();
+        }
     }
     function clickHandler(_event) {
         let target = _event.target;
         if (target.classList.contains("card")) {
             cardsOpen++;
-            if (!(cardsOpen > 2) && target.classList.contains("is_hidden") && target != cardsOpenArray[0]) {
+            if (!(cardsOpen > 2) && target.classList.contains("hidden") && target != cardsOpenArray[0]) {
                 if (target.classList.contains("hidden")) {
-                    target.classList.remove("is_hidden");
+                    target.classList.remove("hidden");
                     target.classList.add("open");
                     cardsOpenArray.push(target);
                 }
@@ -76,8 +80,48 @@ var MemorySettings;
         checkWin();
     }
     function checkWin() {
-        if (checkOthers.length == 0)
-            ;
+        if (checkOthers.length == 0) {
+            alert("Glückwunsch! Falls du nochmal möchtest refreshe einfach die Seite.");
+        }
+    }
+    function shuffleArray(_array) {
+        for (var i = _array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = _array[i];
+            _array[i] = _array[j];
+            _array[j] = temp;
+        }
+        return _array;
+    }
+    function main(_event) {
+        let fieldset = document.querySelector(".formular");
+        if (fieldset.classList.contains("visible")) {
+            fieldset.classList.remove("visible");
+            fieldset.classList.add("is_hidden");
+        }
+        formData = new FormData(document.forms[0]);
+        //console.log(formData);
+        size = Number(formData.get("Slider"));
+        bgColor = formData.get("bgColor");
+        cardColor = formData.get("CardColor");
+        fontColor = formData.get("FontColor");
+        fontStyle = formData.get("radio");
+        let pairOfCards = formData.get("Stepper");
+        if (pairOfCards) {
+            numberPairs = Number(pairOfCards);
+        }
+        else {
+            numberPairs = 5;
+        }
+        for (let i = 0; i < numberPairs; i++) {
+            createCard(cardContent[i]);
+            createCard(cardContent[i]);
+        }
+        shuffleArray(cardArray);
+        for (let i = 0; i < cardArray.length; i++) {
+            let user = document.getElementById("gameArea");
+            user.appendChild(cardArray[i]);
+        }
     }
 })(MemorySettings || (MemorySettings = {}));
 //# sourceMappingURL=Memo.js.map
