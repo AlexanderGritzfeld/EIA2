be2f5250901bbf9c9c87606f4f7ebe8d986162cc
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-04 11:23:11 +0200

    Create canvas_test.ts

---
+++ test&sonstiges/canvas_test.ts
@@ -0,0 +1,17 @@
+namespace test_canvas {
+
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.querySelector("canvas");
+    let crc2: CanvasRenderingContext2D;
+    crc2 = canvas.getContext("2d");
+
+
+    crc2.fillStyle = "#ff0000";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+    crc2.beginPath();
+    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+    crc2.closePath();
+    crc2.strokeStyle = "#ff00ff";
+    crc2.stroke();
+
+}
