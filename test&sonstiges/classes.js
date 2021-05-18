c4f0dd1e5158fe0516a6d629cdaa567ca8e04203
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-18 10:40:22 +0200

    Update classes.js

--- test&sonstiges/classes.js
+++ test&sonstiges/classes.js
@@ -2,9 +2,6 @@
 var classes;
 (function (classes) {
     class Vector {
-        constructor(_x, _y) {
-            this.set(_x, _y);
-        }
         scale(_factor) {
             this.x *= _factor;
             this.y *= _factor;
@@ -12,10 +9,6 @@ var classes;
         add(_addend) {
             this.x += _addend.x;
             this.y += _addend.y;
-        }
-        set(_x, _y) {
-            this.x = 0;
-            this.y = 0;
         }
     }
     let v1 = new Vector();
