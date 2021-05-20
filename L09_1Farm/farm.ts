616504c517f6b789dc795abcfe3f7bcc58161288
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-20 11:28:22 +0200

    Create farm.ts

---
+++ L09_1Farm/farm.ts
@@ -0,0 +1,45 @@
+namespace L09Farm {
+
+    window.addEventListener("load", handleLoad);
+
+     /*export interface FoodAmount {
+        legumes: number;
+        weed: number;
+        corn: number;
+        fish: number;
+        meat: number;
+    }
+
+    var foodAmount: FoodAmount = {
+        legumes: 10,
+        weed: 50,
+        corn: 20,
+        fish: 5,
+        meat: 15
+    }; */
+
+    function handleLoad(): void {
+
+        var pig: Animal = new Animal("pig", "legumes", "Oink!", 10);
+        pig.sing();
+        pig.eat();
+        
+        var cow: Animal = new Animal("cow", "weed", "Muh!", 50);
+        cow.sing();
+        cow.eat();
+        
+        var chicken: Animal = new Animal("chicken", "corn", "Bohkoh!", 20);
+        chicken.sing();
+        chicken.eat();
+        
+        var cat: Animal = new Animal("cat", "fish", "Miau!", 5);
+        cat.sing();
+        cat.eat();
+        
+        var dog: Animal = new Animal("dog", "meat", "Wuff!", 15);
+        dog.sing();
+        dog.eat();
+
+    }
+
+}
