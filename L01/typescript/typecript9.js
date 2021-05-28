70c0658c5e85538991930993deda21f5afd0fc80
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-28 19:57:36 +0200

    Update typecript9.js

--- L01/typescript/typecript9.js
+++ L01/typescript/typecript9.js
@@ -1,23 +1,37 @@
 "use strict";
-var L01T;
-(function (L01T) {
-    //Arrays in denen die Gedichtsfetzen liegen
-    let part1 = ["Das Leben", "Unser Nachbar", "Ein Pinguin", "Anarchie", "Die Welt, so wie wir sie kennen,"];
-    let part2 = ["befreit", "konsumiert", "tötet", "legalisiert", "prophezeit"];
-    let part3 = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "uns"];
-    //jetzt sollen die Arrays geshuffelt werden
-    part1.sort((a, b) => 0.5 - Math.random());
-    part2.sort((a, b) => 0.5 - Math.random());
-    part3.sort((a, b) => 0.5 - Math.random());
-    //jetzt sollen die Fetzen zusammengeführt werden und in der Konsole ausgegeben werden
-    let x = 0;
-    while (x < part1.length) {
-        console.log(part1[x], part2[x], part3[x]);
-        x++;
-    }
-    //wird der Refresh_bttn gedrückt wird die Seite neu geladen
-    document.getElementById("refresh_bttn").addEventListener("click", function () {
-        location.reload();
+/*
+namespace L01T { //damit es nicht rummeckert, dass ich Namen schon vergeben habe
+
+//Arrays in denen die Gedichtsfetzen liegen
+
+let part1: string[] = ["Das Leben", "Unser Nachbar", "Ein Pinguin", "Anarchie", "Die Welt, so wie wir sie kennen,"];
+
+let part2: string[] = ["befreit", "konsumiert", "tötet", "legalisiert", "prophezeit"];
+
+let part3: string[] = ["für dich", "die Leere", "noch einen Pinguin", "das Recht frei zu sein", "uns"];
+
+
+//jetzt sollen die Arrays geshuffelt werden
+
+part1.sort((a, b) => 0.5 - Math.random());
+part2.sort((a, b) => 0.5 - Math.random());
+part3.sort((a, b) => 0.5 - Math.random());
+
+
+//jetzt sollen die Fetzen zusammengeführt werden und in der Konsole ausgegeben werden
+
+let x: number = 0;
+
+while (x < part1.length) {
+    console.log(part1[x], part2[x], part3[x]);
+    x++;
+}
+
+//wird der Refresh_bttn gedrückt wird die Seite neu geladen
+document.getElementById("refresh_bttn").addEventListener("click", function(): void {
+    location.reload();
     });
-})(L01T || (L01T = {})); //Ende Namespace
+
+} //Ende Namespace
+*/
 //# sourceMappingURL=typecript9.js.map
