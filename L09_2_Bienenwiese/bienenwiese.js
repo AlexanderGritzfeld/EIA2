28a4308eb760c8abf0fb890130fafff1a19d8bf3
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-29 17:16:36 +0200

    Update bienenwiese.js

--- L09_2_Bienenwiese/bienenwiese.js
+++ L09_2_Bienenwiese/bienenwiese.js
@@ -1,109 +1,112 @@
 "use strict";
-var L09_2_Bienenwiese;
-(function (L09_2_Bienenwiese) {
+var L09_2;
+(function (L09_2) {
     //inspiriert und teilweise entnommen von Jirka seinem Code "Allay.ts"
-    window.addEventListener("load", handleLoad);
-    let crc2;
-    let golden = 0.62;
+    //erstellt mit der Hilfe von Huu Thien seinem Code
+    L09_2.golden = 0.62;
+    let bees = [];
     let greenColors = ["darkGreen", "forestGreen", "green", "oliveDrab", "seaGreen"];
+    window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        let canvas = document.querySelector("canvas");
+        /*let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
         if (!canvas)
             return;
-        canvas = document.querySelector("canvas");
-        crc2 = canvas.getContext("2d");
+        canvas = <HTMLCanvasElement>document.querySelector("canvas"); */
+        let canvas = document.querySelector("canvas");
+        L09_2.crc2 = canvas.getContext("2d");
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        let horizon = crc2.canvas.height * golden;
+        let horizon = L09_2.crc2.canvas.height * L09_2.golden;
         let posMountains = { x: 0, y: horizon };
         drawBackground();
         drawMountains(posMountains, 75, 200, "grey", "white");
-        drawSun({ x: crc2.canvas.width - 100, y: 75 });
+        drawSun({ x: L09_2.crc2.canvas.width - 100, y: 75 });
         drawTreeLog({ x: 50, y: canvas.height });
         drawLeaves({ x: 175, y: 100 }, { x: 350, y: 100 });
         drawFlower({ x: 400, y: canvas.height - 50 });
-        drawBee({ x: 300, y: 300 });
+        drawBee(10);
+        imageData = L09_2.crc2.getImageData(0, 0, canvas.width, canvas.height);
     }
     function drawBackground() {
-        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        let gradient = L09_2.crc2.createLinearGradient(0, 0, 0, L09_2.crc2.canvas.height);
         gradient.addColorStop(0, "lightblue");
-        gradient.addColorStop(golden, "white");
+        gradient.addColorStop(L09_2.golden, "white");
         gradient.addColorStop(1, "HSL(100, 80%, 30%)");
-        crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        L09_2.crc2.fillStyle = gradient;
+        L09_2.crc2.fillRect(0, 0, L09_2.crc2.canvas.width, L09_2.crc2.canvas.height);
     }
     function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
         let stepMin = 50;
         let stepMax = 150;
         let x = 0;
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.beginPath();
-        crc2.moveTo(0, 0);
-        crc2.lineTo(0, -_max);
+        L09_2.crc2.save();
+        L09_2.crc2.translate(_position.x, _position.y);
+        L09_2.crc2.beginPath();
+        L09_2.crc2.moveTo(0, 0);
+        L09_2.crc2.lineTo(0, -_max);
         do {
             x += stepMin + Math.random() * (stepMax - stepMin);
             let y = -_min - Math.random() * (_max - _min);
-            crc2.lineTo(x, y);
-        } while (x < crc2.canvas.width);
-        crc2.lineTo(x, 0);
-        crc2.closePath();
-        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
+            L09_2.crc2.lineTo(x, y);
+        } while (x < L09_2.crc2.canvas.width);
+        L09_2.crc2.lineTo(x, 0);
+        L09_2.crc2.closePath();
+        let gradient = L09_2.crc2.createLinearGradient(0, 0, 0, -_max);
         gradient.addColorStop(0, _colorLow);
         gradient.addColorStop(0.7, _colorHigh);
-        crc2.fillStyle = gradient;
-        crc2.fill();
-        crc2.restore();
+        L09_2.crc2.fillStyle = gradient;
+        L09_2.crc2.fill();
+        L09_2.crc2.restore();
     }
     function drawSun(_position) {
         let r1 = 30;
         let r2 = 140;
-        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        let gradient = L09_2.crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
         gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
         gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
-        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
+        L09_2.crc2.save();
+        L09_2.crc2.translate(_position.x, _position.y);
+        L09_2.crc2.fillStyle = gradient;
+        L09_2.crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        L09_2.crc2.fill();
+        L09_2.crc2.restore();
     }
     function drawTreeLog(_position) {
         //Hauptstamm
-        crc2.beginPath();
-        crc2.fillStyle = "HSL(32, 100%, 8%)";
-        crc2.moveTo(_position.x, _position.y);
-        crc2.lineTo(_position.x + 100, _position.y);
-        crc2.lineTo(_position.x + 100, 20);
-        crc2.lineTo(_position.x, 20);
-        crc2.closePath();
-        crc2.stroke();
-        crc2.fill();
+        L09_2.crc2.beginPath();
+        L09_2.crc2.fillStyle = "HSL(32, 100%, 8%)";
+        L09_2.crc2.moveTo(_position.x, _position.y);
+        L09_2.crc2.lineTo(_position.x + 100, _position.y);
+        L09_2.crc2.lineTo(_position.x + 100, 20);
+        L09_2.crc2.lineTo(_position.x, 20);
+        L09_2.crc2.closePath();
+        L09_2.crc2.stroke();
+        L09_2.crc2.fill();
         //Ast
-        crc2.beginPath();
-        crc2.fillStyle = "HSL(32, 100%, 8%)";
-        crc2.moveTo(_position.x + 100, 200);
-        crc2.lineTo(_position.x + 300, 100);
-        crc2.lineTo(_position.x + 250, 100);
-        crc2.lineTo(_position.x + 100, 150);
-        crc2.closePath();
-        crc2.fill();
+        L09_2.crc2.beginPath();
+        L09_2.crc2.fillStyle = "HSL(32, 100%, 8%)";
+        L09_2.crc2.moveTo(_position.x + 100, 200);
+        L09_2.crc2.lineTo(_position.x + 300, 100);
+        L09_2.crc2.lineTo(_position.x + 250, 100);
+        L09_2.crc2.lineTo(_position.x + 100, 150);
+        L09_2.crc2.closePath();
+        L09_2.crc2.fill();
         //Bienenstock gelb
-        crc2.save();
-        crc2.beginPath();
-        crc2.translate(_position.x + 180, 190);
-        crc2.fillStyle = "HSL(51, 100%, 50%)";
-        crc2.ellipse(0, 0, 60, 45, 1.5, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
+        L09_2.crc2.save();
+        L09_2.crc2.beginPath();
+        L09_2.crc2.translate(_position.x + 180, 190);
+        L09_2.crc2.fillStyle = "HSL(51, 100%, 50%)";
+        L09_2.crc2.ellipse(0, 0, 60, 45, 1.5, 0, 2 * Math.PI);
+        L09_2.crc2.fill();
+        L09_2.crc2.restore();
         //Schwarz
-        crc2.save();
-        crc2.beginPath();
-        crc2.translate(_position.x + 180, 220);
-        crc2.fillStyle = "black";
-        crc2.ellipse(0, 0, 16, 10.5, 1.5, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
+        L09_2.crc2.save();
+        L09_2.crc2.beginPath();
+        L09_2.crc2.translate(_position.x + 180, 220);
+        L09_2.crc2.fillStyle = "black";
+        L09_2.crc2.ellipse(0, 0, 16, 10.5, 1.5, 0, 2 * Math.PI);
+        L09_2.crc2.fill();
+        L09_2.crc2.restore();
     }
     function drawLeaves(_position, _size) {
         let nParticles = 35;
@@ -111,19 +114,18 @@ var L09_2_Bienenwiese;
         let particle = new Path2D();
         let randomNumberArray = Math.floor(Math.random() * Math.floor(5));
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = "green";
+        L09_2.crc2.save();
+        L09_2.crc2.translate(_position.x, _position.y);
         for (let drawn = 0; drawn < nParticles; drawn++) {
-            crc2.save();
+            L09_2.crc2.save();
             let x = (Math.random() - 0.5) * _size.x;
             let y = -(Math.random() * _size.y);
-            crc2.translate(x, y);
-            crc2.fillStyle = greenColors[randomNumberArray];
-            crc2.fill(particle);
-            crc2.restore();
+            L09_2.crc2.translate(x, y);
+            L09_2.crc2.fillStyle = greenColors[randomNumberArray];
+            L09_2.crc2.fill(particle);
+            L09_2.crc2.restore();
         }
-        crc2.restore();
+        L09_2.crc2.restore();
     }
     function drawFlower(_position) {
         let positionX = _position.x;
@@ -132,79 +134,62 @@ var L09_2_Bienenwiese;
         let randomNumberArray = Math.floor(Math.random() * Math.floor(5));
         for (let flowers = 0; flowers < maxFlowers; flowers++) {
             //Stängel
-            crc2.beginPath();
-            crc2.fillStyle = "HSL(131, 100%, 24%)";
-            crc2.moveTo(positionX, _position.y);
-            crc2.lineTo(positionX + 10, _position.y);
-            crc2.lineTo(positionX + 10, _position.y - 75);
-            crc2.lineTo(positionX, _position.y - 75);
-            crc2.closePath();
-            crc2.fill();
-            console.log(positionX);
+            L09_2.crc2.beginPath();
+            L09_2.crc2.fillStyle = "HSL(131, 100%, 24%)";
+            L09_2.crc2.moveTo(positionX, _position.y);
+            L09_2.crc2.lineTo(positionX + 10, _position.y);
+            L09_2.crc2.lineTo(positionX + 10, _position.y - 75);
+            L09_2.crc2.lineTo(positionX, _position.y - 75);
+            L09_2.crc2.closePath();
+            L09_2.crc2.fill();
             //Blütenblätter
-            crc2.save();
-            crc2.beginPath();
-            crc2.translate(positionX + 15, _position.y - 75);
-            crc2.fillStyle = petalColor[randomNumberArray];
-            crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-            crc2.fill();
-            crc2.restore();
-            crc2.save();
-            crc2.beginPath();
-            crc2.translate(positionX - 5, _position.y - 75);
-            crc2.fillStyle = petalColor[randomNumberArray];
-            crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-            crc2.fill();
-            crc2.restore();
-            crc2.save();
-            crc2.beginPath();
-            crc2.translate(positionX + 5, _position.y - 67);
-            crc2.fillStyle = petalColor[randomNumberArray];
-            crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-            crc2.fill();
-            crc2.restore();
-            crc2.save();
-            crc2.beginPath();
-            crc2.translate(positionX + 5, _position.y - 83);
-            crc2.fillStyle = petalColor[randomNumberArray];
-            crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-            crc2.fill();
-            crc2.restore();
+            L09_2.crc2.save();
+            L09_2.crc2.beginPath();
+            L09_2.crc2.translate(positionX + 15, _position.y - 75);
+            L09_2.crc2.fillStyle = petalColor[randomNumberArray];
+            L09_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+            L09_2.crc2.fill();
+            L09_2.crc2.restore();
+            L09_2.crc2.save();
+            L09_2.crc2.beginPath();
+            L09_2.crc2.translate(positionX - 5, _position.y - 75);
+            L09_2.crc2.fillStyle = petalColor[randomNumberArray];
+            L09_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+            L09_2.crc2.fill();
+            L09_2.crc2.restore();
+            L09_2.crc2.save();
+            L09_2.crc2.beginPath();
+            L09_2.crc2.translate(positionX + 5, _position.y - 67);
+            L09_2.crc2.fillStyle = petalColor[randomNumberArray];
+            L09_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+            L09_2.crc2.fill();
+            L09_2.crc2.restore();
+            L09_2.crc2.save();
+            L09_2.crc2.beginPath();
+            L09_2.crc2.translate(positionX + 5, _position.y - 83);
+            L09_2.crc2.fillStyle = petalColor[randomNumberArray];
+            L09_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+            L09_2.crc2.fill();
+            L09_2.crc2.restore();
             //Blüte innen
-            crc2.save();
-            crc2.beginPath();
-            crc2.translate(positionX + 5, _position.y - 75);
-            crc2.fillStyle = "yellow";
-            crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-            crc2.fill();
-            crc2.restore();
+            L09_2.crc2.save();
+            L09_2.crc2.beginPath();
+            L09_2.crc2.translate(positionX + 5, _position.y - 75);
+            L09_2.crc2.fillStyle = "yellow";
+            L09_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+            L09_2.crc2.fill();
+            L09_2.crc2.restore();
             positionX = positionX + 100;
         } //Ende for Schleife
     }
-    function drawBee(_position) {
-        //body yellow
-        crc2.save();
-        crc2.beginPath();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = "yellow";
-        crc2.ellipse(0, 0, 16, 10.5, 3.1, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.strokeStyle = "black";
-        crc2.stroke();
-        crc2.restore();
-        //body black
-        crc2.save();
-        crc2.beginPath();
-        //crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = "black";
-        crc2.moveTo(_position.x - 4, _position.y + 10);
-        crc2.lineTo(_position.x + 4, _position.y + 10);
-        crc2.lineTo(_position.x + 4, _position.y - 10);
-        crc2.lineTo(_position.x - 4, _position.y - 10);
-        //crc2.lineTo(_position.x + 4, _position.y + 10);
-        crc2.closePath();
-        crc2.fill();
-        crc2.restore();
+    function drawBee(_nBees) {
+        for (let i = 0; i < _nBees; i++) {
+            console.log("Test");
+            let randomScale = 0.5 + Math.random() * (2.5 - 1.3);
+            let randomVelocityX = (Math.random() - 0.5) * 5;
+            let randomVelocityY = (Math.random() - 0.5) * 5;
+            bees.push(new L09_2.Bees({ x: L09_2.crc2.canvas.width / 2, y: L09_2.crc2.canvas.height * L09_2.golden }, { x: randomVelocityX, y: randomVelocityY }, randomScale));
+        }
     }
-})(L09_2_Bienenwiese || (L09_2_Bienenwiese = {}));
+})(L09_2 || (L09_2 = {}));
 //# sourceMappingURL=bienenwiese.js.map
