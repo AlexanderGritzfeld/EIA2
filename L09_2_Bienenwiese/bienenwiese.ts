00b52f96aabafefda60a11242809d86554db40f7
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-05-30 18:01:46 +0200

    Ups da war wohl etwas doppelt

--- L09_2_Bienenwiese/bienenwiese.ts
+++ L09_2_Bienenwiese/bienenwiese.ts
@@ -21,12 +21,12 @@ namespace L09_2 {
 
     window.addEventListener("load", handleLoad);
 
-    function handleLoad(_event: Event): void {
+    function handleLoad(): void {
         let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
         if (!canvas)
             return;
         canvas = <HTMLCanvasElement>document.querySelector("canvas");
-        document.querySelector("canvas");   
+        //document.querySelector("canvas");   
         crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
         canvas.width = window.innerWidth;
