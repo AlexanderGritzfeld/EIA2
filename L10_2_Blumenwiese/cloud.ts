2bd18a2d58747def723a22b3e88b4c32ecec8848
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-12 09:57:51 +0200

    Update cloud.ts

--- L10_2_Blumenwiese/cloud.ts
+++ L10_2_Blumenwiese/cloud.ts
@@ -1,27 +1,27 @@
 namespace L10_2 {
 
-    export class Clouds {
-        posX: number;
+    export class Clouds extends Movable {
+        
+        /*posX: number;
         posY: number; 
-        velocityX: number = 0.5;
+        velocityX: number = 0.5; */
 
-        constructor(_position: Vector) {
-            this.posY = _position.y;
-            this.posX = _position.x;
+        constructor(_position: Vector, _velocity: Vector) {
+            super(_position, _velocity);
         }
 
         draw(): void {
 
             crc2.save();
-            crc2.translate(this.posX, this.posY);
+            crc2.translate(this.startPosX, this.startPosY);
 
             crc2.beginPath();
-            crc2.arc(this.posX, this.posY, 60, Math.PI * 0.5, Math.PI * 1.5);
-            crc2.arc(this.posX + 70, this.posY - 60, 70, Math.PI * 1, Math.PI * 1.85);
-            crc2.arc(this.posX + 152, this.posY - 45, 50, Math.PI * 1.37, Math.PI * 1.91);
-            crc2.arc(this.posX + 200, this.posY, 60, Math.PI * 1.5, Math.PI * 0.5);
-            crc2.moveTo(this.posX + 200, this.posY + 60);
-            crc2.lineTo(this.posX, this.posY + 60);
+            crc2.arc(this.startPosX, this.startPosY, 60, Math.PI * 0.5, Math.PI * 1.5);
+            crc2.arc(this.startPosX + 70, this.startPosY - 60, 70, Math.PI * 1, Math.PI * 1.85);
+            crc2.arc(this.startPosX + 152, this.startPosY - 45, 50, Math.PI * 1.37, Math.PI * 1.91);
+            crc2.arc(this.startPosX + 200, this.startPosY, 60, Math.PI * 1.5, Math.PI * 0.5);
+            crc2.moveTo(this.startPosX + 200, this.startPosY + 60);
+            crc2.lineTo(this.startPosX, this.startPosY + 60);
             crc2.strokeStyle = "HSLA(0, 10%, 90%, 0.4)";
             crc2.stroke();
             crc2.fillStyle = "HSLA(0, 0%, 100%, 0.4)";
@@ -32,13 +32,13 @@ namespace L10_2 {
 
         update(): void {
            
-            if (this.posX > crc2.canvas.width || this.posX < 0) {
+            if (this.startPosX > crc2.canvas.width || this.startPosX < 0) {
                 this.velocityX = -this.velocityX;
             }
 
-            this.posX += this.velocityX;
+            this.startPosX += this.velocityX;
             
-            this.draw();
+            //this.draw();
         }
 
     }//Ende class
