98edbeb10c7e175f6419a0b1cd4764b7c3e6522e
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-12 09:58:04 +0200

    Update bienenwiese.js

--- L10_2_Blumenwiese/bienenwiese.js
+++ L10_2_Blumenwiese/bienenwiese.js
@@ -2,11 +2,11 @@
 var L10_2;
 (function (L10_2) {
     //inspiriert und teilweise entnommen von Jirka seinem Code "Allay.ts"
-    console.log("Test");
     L10_2.golden = 0.62;
-    let clouds = [];
+    //let clouds: Clouds[] = [];
     let flowers = [];
-    let bees = [];
+    //let bees: Bees[] = [];
+    let movable = [];
     let imageData;
     L10_2.greenColors = ["darkGreen", "forestGreen", "green", "oliveDrab", "seaGreen"];
     window.addEventListener("load", handleLoad);
@@ -15,13 +15,12 @@ var L10_2;
         if (!canvas)
             return;
         canvas = document.querySelector("canvas");
-        //document.querySelector("canvas");   
         L10_2.crc2 = canvas.getContext("2d");
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         drawBackground();
-        drawCloud();
         drawTree();
+        drawCloud();
         drawFlower();
         drawBee(10);
         imageData = L10_2.crc2.getImageData(0, 0, canvas.width, canvas.height);
@@ -35,7 +34,7 @@ var L10_2;
         L10_2.drawSun({ x: L10_2.crc2.canvas.width - 100, y: 75 });
     }
     function drawCloud() {
-        clouds.push(new L10_2.Clouds({ x: L10_2.crc2.canvas.width * .10, y: L10_2.crc2.canvas.height * .10 }));
+        movable.push(new L10_2.Clouds({ x: L10_2.crc2.canvas.width * .10, y: L10_2.crc2.canvas.height * .10 }, { x: 0.5, y: 0.1 }));
     }
     function drawTree() {
         L10_2.drawTreeLog({ x: 50, y: L10_2.crc2.canvas.height });
@@ -46,22 +45,20 @@ var L10_2;
     }
     function drawBee(_nBees) {
         for (let i = 0; i < _nBees; i++) {
-            let randomScale = 0.5 + Math.random() * (2.5 - 1.3);
+            //let randomScale: number = 0.5 + Math.random() * (2.5 - 1.3);
             let randomVelocityX = (Math.random() - 0.5) * 5;
             let randomVelocityY = (Math.random() - 0.5) * 5;
-            bees.push(new L10_2.Bees({ x: L10_2.crc2.canvas.width / 4, y: L10_2.crc2.canvas.height * L10_2.golden / 2.4 }, { x: randomVelocityX, y: randomVelocityY }, randomScale));
+            movable.push(new L10_2.Bees({ x: L10_2.crc2.canvas.width / 4, y: L10_2.crc2.canvas.height * L10_2.golden / 2.4 }, { x: randomVelocityX, y: randomVelocityY })); //, randomScale
         }
     }
     function animate() {
         requestAnimationFrame(animate);
         L10_2.crc2.clearRect(0, 0, L10_2.crc2.canvas.width, L10_2.crc2.canvas.height);
         L10_2.crc2.putImageData(imageData, 0, 0);
-        for (let index = 0; index < bees.length; index++) {
-            bees[index].update();
-        }
-        for (let index = 0; index < clouds.length; index++) {
-            clouds[index].update();
-        }
-    }
+        for (let index = 0; index < movable.length; index++) {
+            movable[index].update();
+            movable[index].draw();
+        } // Ende for
+    } //Ende animate
 })(L10_2 || (L10_2 = {}));
 //# sourceMappingURL=bienenwiese.js.map
