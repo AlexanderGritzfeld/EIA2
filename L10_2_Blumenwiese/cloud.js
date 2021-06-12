6220ba9af98f86bd314e87f67e041b400733af76
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-12 09:57:56 +0200

    Update cloud.js

--- L10_2_Blumenwiese/cloud.js
+++ L10_2_Blumenwiese/cloud.js
@@ -1,22 +1,23 @@
 "use strict";
 var L10_2;
 (function (L10_2) {
-    class Clouds {
-        constructor(_position) {
-            this.velocityX = 0.5;
-            this.posY = _position.y;
-            this.posX = _position.x;
+    class Clouds extends L10_2.Movable {
+        /*posX: number;
+        posY: number;
+        velocityX: number = 0.5; */
+        constructor(_position, _velocity) {
+            super(_position, _velocity);
         }
         draw() {
             L10_2.crc2.save();
-            L10_2.crc2.translate(this.posX, this.posY);
+            L10_2.crc2.translate(this.startPosX, this.startPosY);
             L10_2.crc2.beginPath();
-            L10_2.crc2.arc(this.posX, this.posY, 60, Math.PI * 0.5, Math.PI * 1.5);
-            L10_2.crc2.arc(this.posX + 70, this.posY - 60, 70, Math.PI * 1, Math.PI * 1.85);
-            L10_2.crc2.arc(this.posX + 152, this.posY - 45, 50, Math.PI * 1.37, Math.PI * 1.91);
-            L10_2.crc2.arc(this.posX + 200, this.posY, 60, Math.PI * 1.5, Math.PI * 0.5);
-            L10_2.crc2.moveTo(this.posX + 200, this.posY + 60);
-            L10_2.crc2.lineTo(this.posX, this.posY + 60);
+            L10_2.crc2.arc(this.startPosX, this.startPosY, 60, Math.PI * 0.5, Math.PI * 1.5);
+            L10_2.crc2.arc(this.startPosX + 70, this.startPosY - 60, 70, Math.PI * 1, Math.PI * 1.85);
+            L10_2.crc2.arc(this.startPosX + 152, this.startPosY - 45, 50, Math.PI * 1.37, Math.PI * 1.91);
+            L10_2.crc2.arc(this.startPosX + 200, this.startPosY, 60, Math.PI * 1.5, Math.PI * 0.5);
+            L10_2.crc2.moveTo(this.startPosX + 200, this.startPosY + 60);
+            L10_2.crc2.lineTo(this.startPosX, this.startPosY + 60);
             L10_2.crc2.strokeStyle = "HSLA(0, 10%, 90%, 0.4)";
             L10_2.crc2.stroke();
             L10_2.crc2.fillStyle = "HSLA(0, 0%, 100%, 0.4)";
@@ -24,11 +25,11 @@ var L10_2;
             L10_2.crc2.restore();
         }
         update() {
-            if (this.posX > L10_2.crc2.canvas.width || this.posX < 0) {
+            if (this.startPosX > L10_2.crc2.canvas.width || this.startPosX < 0) {
                 this.velocityX = -this.velocityX;
             }
-            this.posX += this.velocityX;
-            this.draw();
+            this.startPosX += this.velocityX;
+            //this.draw();
         }
     } //Ende class
     L10_2.Clouds = Clouds;
