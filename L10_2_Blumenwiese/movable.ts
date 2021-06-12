a16689cc9883cb6fb96806a33b4696a2db5fc171
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-12 09:57:43 +0200

    Create movable.ts

---
+++ L10_2_Blumenwiese/movable.ts
@@ -0,0 +1,31 @@
+namespace L10_2 {
+
+    export class Movable {
+        
+        startPosX: number;
+        startPosY: number;
+
+        velocityX: number;
+        velocityY: number;
+
+        constructor(_position: Vector, _velocity: Vector) {
+            
+            this.startPosX = _position.x;
+            this.startPosY = _position.y;
+
+            this.velocityX = _velocity.x;
+            this.velocityY = _velocity.y;
+        }
+
+        draw(): void {
+            //draw
+        }
+
+        update(): void {
+
+        //update
+        }
+        
+    }
+
+}
