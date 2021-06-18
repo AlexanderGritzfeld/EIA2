556fc849344a4b2e15e535427646e87640fe7546
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-18 21:17:44 +0200

    Create movable.js

---
+++ L11_1_BlumenwieseAdvanced/movable.js
@@ -0,0 +1,20 @@
+"use strict";
+var L10_2;
+(function (L10_2) {
+    class Movable {
+        constructor(_position, _velocity) {
+            this.startPosX = _position.x;
+            this.startPosY = _position.y;
+            this.velocityX = _velocity.x;
+            this.velocityY = _velocity.y;
+        }
+        draw() {
+            //draw
+        }
+        update() {
+            //update
+        }
+    }
+    L10_2.Movable = Movable;
+})(L10_2 || (L10_2 = {}));
+//# sourceMappingURL=movable.js.map
