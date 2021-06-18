ec07b8302b8c521522f9a4f6ef24ac3097355ca5
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-18 21:25:13 +0200

    Update bees.js

--- L11_1_BlumenwieseAdvanced/bees.js
+++ L11_1_BlumenwieseAdvanced/bees.js
@@ -1,7 +1,7 @@
 "use strict";
-var L10_2;
-(function (L10_2) {
-    class Bees extends L10_2.Movable {
+var L11_1;
+(function (L11_1) {
+    class Bees extends L11_1.Movable {
         constructor(_position, _velocity) {
             super(_position, _velocity);
             this.randomNumber = (Math.floor(Math.random() * 2000) + 1000);
@@ -9,51 +9,51 @@ var L10_2;
         }
         draw() {
             //wings
-            L10_2.crc2.save();
-            L10_2.crc2.translate(this.startPosX - 6, this.startPosY - 10);
-            L10_2.crc2.fillStyle = "HSLA(178, 100%, 50%, 0.7)";
-            L10_2.crc2.strokeStyle = "black";
-            L10_2.crc2.beginPath();
-            L10_2.crc2.arc(0, 0, 8, 0, 2 * Math.PI);
-            L10_2.crc2.stroke();
-            L10_2.crc2.fill();
-            L10_2.crc2.restore();
-            L10_2.crc2.save();
-            L10_2.crc2.beginPath();
-            L10_2.crc2.fillStyle = "HSLA(178, 100%, 50%, 0.7)";
-            L10_2.crc2.strokeStyle = "black";
-            L10_2.crc2.translate(this.startPosX + 6, this.startPosY - 10);
-            L10_2.crc2.arc(0, 0, 8, 0, 2 * Math.PI);
-            L10_2.crc2.stroke();
-            L10_2.crc2.fill();
-            L10_2.crc2.restore();
+            L11_1.crc2.save();
+            L11_1.crc2.translate(this.startPosX - 6, this.startPosY - 10);
+            L11_1.crc2.fillStyle = "HSLA(178, 100%, 50%, 0.7)";
+            L11_1.crc2.strokeStyle = "black";
+            L11_1.crc2.beginPath();
+            L11_1.crc2.arc(0, 0, 8, 0, 2 * Math.PI);
+            L11_1.crc2.stroke();
+            L11_1.crc2.fill();
+            L11_1.crc2.restore();
+            L11_1.crc2.save();
+            L11_1.crc2.beginPath();
+            L11_1.crc2.fillStyle = "HSLA(178, 100%, 50%, 0.7)";
+            L11_1.crc2.strokeStyle = "black";
+            L11_1.crc2.translate(this.startPosX + 6, this.startPosY - 10);
+            L11_1.crc2.arc(0, 0, 8, 0, 2 * Math.PI);
+            L11_1.crc2.stroke();
+            L11_1.crc2.fill();
+            L11_1.crc2.restore();
             //body yellow
-            L10_2.crc2.save();
-            L10_2.crc2.beginPath();
-            L10_2.crc2.translate(this.startPosX, this.startPosY);
-            L10_2.crc2.fillStyle = "yellow";
-            L10_2.crc2.ellipse(0, 0, 16, 10.5, 3.1, 0, 2 * Math.PI);
-            L10_2.crc2.fill();
-            L10_2.crc2.strokeStyle = "black";
-            L10_2.crc2.stroke();
-            L10_2.crc2.restore();
+            L11_1.crc2.save();
+            L11_1.crc2.beginPath();
+            L11_1.crc2.translate(this.startPosX, this.startPosY);
+            L11_1.crc2.fillStyle = "yellow";
+            L11_1.crc2.ellipse(0, 0, 16, 10.5, 3.1, 0, 2 * Math.PI);
+            L11_1.crc2.fill();
+            L11_1.crc2.strokeStyle = "black";
+            L11_1.crc2.stroke();
+            L11_1.crc2.restore();
             //body black
             //crc2.save();
-            L10_2.crc2.beginPath();
-            L10_2.crc2.fillStyle = "black";
-            L10_2.crc2.moveTo(this.startPosX - 4, this.startPosY + 10);
-            L10_2.crc2.lineTo(this.startPosX + 4, this.startPosY + 10);
-            L10_2.crc2.lineTo(this.startPosX + 4, this.startPosY - 10);
-            L10_2.crc2.lineTo(this.startPosX - 4, this.startPosY - 10);
-            L10_2.crc2.closePath();
-            L10_2.crc2.fill();
-            L10_2.crc2.restore();
+            L11_1.crc2.beginPath();
+            L11_1.crc2.fillStyle = "black";
+            L11_1.crc2.moveTo(this.startPosX - 4, this.startPosY + 10);
+            L11_1.crc2.lineTo(this.startPosX + 4, this.startPosY + 10);
+            L11_1.crc2.lineTo(this.startPosX + 4, this.startPosY - 10);
+            L11_1.crc2.lineTo(this.startPosX - 4, this.startPosY - 10);
+            L11_1.crc2.closePath();
+            L11_1.crc2.fill();
+            L11_1.crc2.restore();
         }
         update() {
-            if (this.startPosX > L10_2.crc2.canvas.width || this.startPosX < 0) {
+            if (this.startPosX > L11_1.crc2.canvas.width || this.startPosX < 0) {
                 this.velocityX = -this.velocityX;
             }
-            if (this.startPosY > L10_2.crc2.canvas.height || this.startPosY < L10_2.crc2.canvas.height * 0.25) {
+            if (this.startPosY > L11_1.crc2.canvas.height || this.startPosY < L11_1.crc2.canvas.height * 0.25) {
                 this.velocityY = -this.velocityY;
             }
             if (this.counter == this.randomNumber) {
@@ -68,6 +68,6 @@ var L10_2;
             //this.draw();
         }
     } //Ende export class
-    L10_2.Bees = Bees;
-})(L10_2 || (L10_2 = {}));
+    L11_1.Bees = Bees;
+})(L11_1 || (L11_1 = {}));
 //# sourceMappingURL=bees.js.map
