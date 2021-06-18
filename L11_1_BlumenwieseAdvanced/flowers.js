2d03f301dd8e0531a5a109dd952f1b5ea1e5c8a3
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-06-18 21:25:30 +0200

    Update flowers.js

--- L11_1_BlumenwieseAdvanced/flowers.js
+++ L11_1_BlumenwieseAdvanced/flowers.js
@@ -1,6 +1,6 @@
 "use strict";
-var L10_2;
-(function (L10_2) {
+var L11_1;
+(function (L11_1) {
     class Flowers {
         constructor(_position) {
             this.petalColor = ["red", "white", "orange", "HSL(329, 100%, 24%)", "purple"];
@@ -12,57 +12,57 @@ var L10_2;
         draw() {
             for (let flowers = 0; flowers < 11; flowers++) {
                 //Stängel
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.fillStyle = "HSL(131, 100%, 24%)";
-                L10_2.crc2.moveTo(this.positionX, this.positionY);
-                L10_2.crc2.lineTo(this.positionX + 10, this.positionY);
-                L10_2.crc2.lineTo(this.positionX + 10, this.positionY - 75);
-                L10_2.crc2.lineTo(this.positionX, this.positionY - 75);
-                L10_2.crc2.closePath();
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.fillStyle = "HSL(131, 100%, 24%)";
+                L11_1.crc2.moveTo(this.positionX, this.positionY);
+                L11_1.crc2.lineTo(this.positionX + 10, this.positionY);
+                L11_1.crc2.lineTo(this.positionX + 10, this.positionY - 75);
+                L11_1.crc2.lineTo(this.positionX, this.positionY - 75);
+                L11_1.crc2.closePath();
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
                 //Blütenblätter
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.translate(this.positionX + 15, this.positionY - 75);
-                L10_2.crc2.fillStyle = this.petalColor[this.randomNumberArray];
-                L10_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.translate(this.positionX - 5, this.positionY - 75);
-                L10_2.crc2.fillStyle = this.petalColor[this.randomNumberArray];
-                L10_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.translate(this.positionX + 5, this.positionY - 67);
-                L10_2.crc2.fillStyle = this.petalColor[this.randomNumberArray];
-                L10_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.translate(this.positionX + 5, this.positionY - 83);
-                L10_2.crc2.fillStyle = this.petalColor[this.randomNumberArray];
-                L10_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.translate(this.positionX + 15, this.positionY - 75);
+                L11_1.crc2.fillStyle = this.petalColor[this.randomNumberArray];
+                L11_1.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.translate(this.positionX - 5, this.positionY - 75);
+                L11_1.crc2.fillStyle = this.petalColor[this.randomNumberArray];
+                L11_1.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.translate(this.positionX + 5, this.positionY - 67);
+                L11_1.crc2.fillStyle = this.petalColor[this.randomNumberArray];
+                L11_1.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.translate(this.positionX + 5, this.positionY - 83);
+                L11_1.crc2.fillStyle = this.petalColor[this.randomNumberArray];
+                L11_1.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
                 //Blüte innen
-                L10_2.crc2.save();
-                L10_2.crc2.beginPath();
-                L10_2.crc2.translate(this.positionX + 5, this.positionY - 75);
-                L10_2.crc2.fillStyle = "yellow";
-                L10_2.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
-                L10_2.crc2.fill();
-                L10_2.crc2.restore();
+                L11_1.crc2.save();
+                L11_1.crc2.beginPath();
+                L11_1.crc2.translate(this.positionX + 5, this.positionY - 75);
+                L11_1.crc2.fillStyle = "yellow";
+                L11_1.crc2.arc(0, 0, 15, 0, 2 * Math.PI);
+                L11_1.crc2.fill();
+                L11_1.crc2.restore();
                 this.positionX = this.positionX + 100;
             } //Ende for Schleife
         }
     } //Ende class Flowers
-    L10_2.Flowers = Flowers;
-})(L10_2 || (L10_2 = {}));
+    L11_1.Flowers = Flowers;
+})(L11_1 || (L11_1 = {}));
 //# sourceMappingURL=flowers.js.map
