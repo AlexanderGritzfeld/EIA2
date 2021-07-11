f290847bc35f25eb24b97fab876c7c908bf3b611
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-11 21:14:35 +0200

    Player werden nun gemalt (Proto)

--- Abschluss/Code/Player.ts
+++ Abschluss/Code/Player.ts
@@ -1,7 +1,30 @@
 namespace SoSe21 {
 
     export class Player extends Movable {
-        
+
+        constructor(_position: Vector, _velocity: Vector) { //_randomScale: number
+            
+            super(_position, _velocity);
+
+        } //end constructor
+
+        draw(): void {
+
+            console.log("Test");
+
+            crc2.save();
+            crc2.translate(this.startPosX, this.startPosY);
+
+            crc2.beginPath();
+            crc2.strokeStyle = "black";
+            crc2.fillStyle = "red";
+            crc2.lineWidth = 2;
+            crc2.arc(this.startPosX, this.startPosY, 50, 0, 2 * Math.PI);
+            crc2.stroke(); 
+            crc2.fill();
+            crc2.restore();
+
+        }
 
     } //end class Player
 
