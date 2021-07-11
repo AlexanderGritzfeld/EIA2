026608ff91c11855c22cf8e94b237786070b27a9
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-11 21:10:27 +0200

    Update AbschlussarbeitSoSe21.js

--- Abschluss/Code/AbschlussarbeitSoSe21.js
+++ Abschluss/Code/AbschlussarbeitSoSe21.js
@@ -11,13 +11,21 @@ var SoSe21;
         SoSe21.crc2 = canvas.getContext("2d");
         drawBackground();
         drawTeam(11);
+        animate();
     } //end handleLoad
     function drawBackground() {
         SoSe21.drawField();
     }
     function drawTeam(_nPlayers) {
         for (let i = 0; i <= _nPlayers; i++) {
-            movable.push(new SoSe21.Player());
+            movable.push(new SoSe21.Player({ x: SoSe21.crc2.canvas.width / (i + 1), y: SoSe21.crc2.canvas.height / (i + 1) }, { x: 0.5, y: 0.5 }));
+            console.log("Spieler Nummer: " + (i + 1));
+        } //end for 
+    } //end drawTeam
+    function animate() {
+        for (let index = 0; index < movable.length; index++) {
+            movable[index].update();
+            movable[index].draw();
         }
     }
 })(SoSe21 || (SoSe21 = {})); //end namespace
