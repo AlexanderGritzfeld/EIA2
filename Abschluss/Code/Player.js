5dceb7d1279453582525bdb9de3baf0a92909db2
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-11 21:13:36 +0200

    Update Player.js

--- Abschluss/Code/Player.js
+++ Abschluss/Code/Player.js
@@ -2,6 +2,22 @@
 var SoSe21;
 (function (SoSe21) {
     class Player extends SoSe21.Movable {
+        constructor(_position, _velocity) {
+            super(_position, _velocity);
+        } //end constructor
+        draw() {
+            console.log("Test");
+            SoSe21.crc2.save();
+            SoSe21.crc2.translate(this.startPosX, this.startPosY);
+            SoSe21.crc2.beginPath();
+            SoSe21.crc2.strokeStyle = "black";
+            SoSe21.crc2.fillStyle = "red";
+            SoSe21.crc2.lineWidth = 2;
+            SoSe21.crc2.arc(this.startPosX, this.startPosY, 50, 0, 2 * Math.PI);
+            SoSe21.crc2.stroke();
+            SoSe21.crc2.fill();
+            SoSe21.crc2.restore();
+        }
     } //end class Player
     SoSe21.Player = Player;
 })(SoSe21 || (SoSe21 = {}));
