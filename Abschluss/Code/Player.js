74e9d4d43b8468e098dff9f600f6c66597234996
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-11 18:55:11 +0200

    Create Player.js

---
+++ Abschluss/Code/Player.js
@@ -0,0 +1,8 @@
+"use strict";
+var SoSe21;
+(function (SoSe21) {
+    class Player extends SoSe21.Movable {
+    } //end class Player
+    SoSe21.Player = Player;
+})(SoSe21 || (SoSe21 = {}));
+//# sourceMappingURL=Player.js.map
