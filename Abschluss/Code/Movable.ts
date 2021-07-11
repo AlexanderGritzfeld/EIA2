c57240312d2e1f721432e4d18aa365edefe2bda9
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-11 18:55:08 +0200

    Movable für Spieler und Co

---
+++ Abschluss/Code/Movable.ts
@@ -0,0 +1,36 @@
+namespace SoSe21 {
+
+    export class Movable {
+        
+        startPosX: number;
+        startPosY: number;
+
+        velocityX: number;
+        velocityY: number;
+
+        constructor(_position: Vector, _velocity: Vector) {
+            
+            this.startPosX = _position.x;
+            this.startPosY = _position.y;
+
+            this.velocityX = _velocity.x;
+            this.velocityY = _velocity.y;
+        }
+
+        draw(): void {
+            //draw
+        }
+
+        /*brauche ich das?
+        pickColor(): void {
+            //pick color
+        }
+        */
+
+        update(): void {
+        //update
+        }
+        
+    } //end class Mivable
+
+}
