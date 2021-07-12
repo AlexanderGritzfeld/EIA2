e10d13fb9f31ba9fbf8a48774a17265813cc90f4
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-12 21:10:22 +0200

    Update AbschlussarbeitSoSe21.js

--- Abschluss/Code/AbschlussarbeitSoSe21.js
+++ Abschluss/Code/AbschlussarbeitSoSe21.js
@@ -9,10 +9,35 @@ var SoSe21;
             return;
         canvas = document.querySelector("canvas");
         SoSe21.crc2 = canvas.getContext("2d");
+        /*
+        document.addEventListener ("mousemove", setInfoBox);
+        document.addEventListener ("click", logInfo);
+        document.body.addEventListener ("click", logInfo);
+        */
         drawBackground();
         drawTeam(11);
         animate();
     } //end handleLoad
+    /*
+    function setInfoBox(_event: MouseEvent): void {
+        let spanElement: HTMLSpanElement = <HTMLSpanElement>document.querySelector("#span");
+    
+        let x: number = _event.clientX;
+        let y: number = _event.clientY;
+        let mousePosition: string = "X-Coordinates: " + x + "," + "<br>" + "Y-Coordinates: " + y + ",";
+        let eventTarget: EventTarget = <EventTarget>_event.target;
+    
+        spanElement.innerHTML = mousePosition + "<br>" + eventTarget;
+        spanElement.style.top = y + 8 + "px";
+        spanElement.style.left = x + 10 + "px";
+    }
+    function logInfo(_event: Event): void {
+
+        console.info("type: " + _event.type);
+        console.log("target " + _event.target);
+        console.log("current target " + _event.currentTarget);
+        console.log("composed path " + _event.composedPath());
+    } */
     function drawBackground() {
         SoSe21.drawField();
     }
@@ -32,7 +57,7 @@ var SoSe21;
            movable[index].draw();
 
        } //end for */
-        while (SoSe21.j < (movable.length - 1)) {
+        while (SoSe21.j < 11) { //
             movable[SoSe21.j].update();
             movable[SoSe21.j].draw();
             SoSe21.j++;
