309f5cb8d32371ba39dc014b4d5a664b3e186145
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-18 21:04:14 +0200

    Create OtherHuman.js

---
+++ Abschluss/Code/OtherHuman.js
@@ -0,0 +1,23 @@
+"use strict";
+var SoSe21;
+(function (SoSe21) {
+    class OtherHuman extends SoSe21.Movable {
+        constructor(_position, _velocity) {
+            super(_position, _velocity);
+            this.positionOther = _position;
+        } //Ende constructor
+        draw() {
+            SoSe21.crc2.save();
+            SoSe21.crc2.beginPath();
+            SoSe21.crc2.strokeStyle = "black";
+            SoSe21.crc2.fillStyle = "#808080";
+            SoSe21.crc2.lineWidth = 2;
+            SoSe21.crc2.arc(this.positionOther.x, this.positionOther.y, 7, 0, 2 * Math.PI);
+            SoSe21.crc2.stroke();
+            SoSe21.crc2.fill();
+            SoSe21.crc2.restore();
+        } //ende draw
+    }
+    SoSe21.OtherHuman = OtherHuman;
+})(SoSe21 || (SoSe21 = {}));
+//# sourceMappingURL=OtherHuman.js.map
