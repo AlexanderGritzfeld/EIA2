85034e41801d088ff8fdbd042d3864ad13f1029c
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-18 21:23:06 +0200

    Create Ball.js

---
+++ Abschluss/Code/Ball.js
@@ -0,0 +1,11 @@
+"use strict";
+var SoSe21;
+(function (SoSe21) {
+    class Ball extends SoSe21.Movable {
+        constructor(_position, _velocity) {
+            super(_position, _velocity);
+        } // end constructor
+    }
+    SoSe21.Ball = Ball;
+})(SoSe21 || (SoSe21 = {}));
+//# sourceMappingURL=Ball.js.map
