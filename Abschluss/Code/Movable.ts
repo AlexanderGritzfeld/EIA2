e954e592576738848bd17f187926c7f450e3f102
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 23:16:01 +0200

    Kommis raus

--- Abschluss/Code/Movable.ts
+++ Abschluss/Code/Movable.ts
@@ -1,18 +1,11 @@
 namespace SoSe21 {
 
     export abstract class Movable {
-        
-        /* brauche ich das?
-        startPosX: number;
-        startPosY: number; */
 
         protected velocity: number;
         protected position: Vector;
 
         constructor(_position: Vector, _velocity: number) {
-            
-            /*this.startPosX = _position.x;
-            this.startPosY = _position.y; */
 
             this.position = _position;
 
