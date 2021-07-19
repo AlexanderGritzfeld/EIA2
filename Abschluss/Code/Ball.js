78b8f7c55fbf1d391a7be61a0fa5168bd92cccac
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:07:10 +0200

    Update Ball.js

--- Abschluss/Code/Ball.js
+++ Abschluss/Code/Ball.js
@@ -4,7 +4,7 @@ var SoSe21;
     class Ball extends SoSe21.Movable {
         constructor(_position, _velocity) {
             super(_position, _velocity);
-            this.positionBall = _position;
+            //this.positionBall = _position;
         } // end constructor
         draw() {
             SoSe21.crc2.save();
@@ -12,7 +12,7 @@ var SoSe21;
             SoSe21.crc2.strokeStyle = "black";
             SoSe21.crc2.fillStyle = "#f2f2f2";
             SoSe21.crc2.lineWidth = 2;
-            SoSe21.crc2.arc(this.positionBall.x, this.positionBall.y, 5, 0, 2 * Math.PI);
+            SoSe21.crc2.arc(this.position.x, this.position.y, 5, 0, 2 * Math.PI);
             SoSe21.crc2.stroke();
             SoSe21.crc2.fill();
             SoSe21.crc2.restore();
