b08e3280ce52ee0305da7da4708bf8f994475097
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:07:47 +0200

    Update Movable.js

--- Abschluss/Code/Movable.js
+++ Abschluss/Code/Movable.js
@@ -3,23 +3,18 @@ var SoSe21;
 (function (SoSe21) {
     class Movable {
         constructor(_position, _velocity) {
-            this.startPosX = _position.x;
-            this.startPosY = _position.y;
-            this.velocityX = _velocity.x;
-            this.velocityY = _velocity.y;
+            /*this.startPosX = _position.x;
+            this.startPosY = _position.y; */
+            this.position = _position;
+            this.velocity = _velocity;
         }
         draw() {
             //draw
         }
-        /*brauche ich das?
-        pickColor(): void {
-            //pick color
-        }
-        */
         update() {
             //update
         }
-    } //end class Mivable
+    } //end class Movable
     SoSe21.Movable = Movable;
 })(SoSe21 || (SoSe21 = {}));
 //# sourceMappingURL=Movable.js.map
