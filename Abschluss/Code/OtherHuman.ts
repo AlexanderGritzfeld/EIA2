af5de98d6ca88fade72b74cd2be9020d037f7d9d
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:09:27 +0200

    velocity und position angepasst

--- Abschluss/Code/OtherHuman.ts
+++ Abschluss/Code/OtherHuman.ts
@@ -4,11 +4,11 @@ namespace SoSe21 {
 
         public positionOther: Vector;
 
-        constructor(_position: Vector, _velocity: Vector) {
+        constructor(_position: Vector, _velocity: number) {
             
             super(_position, _velocity);
 
-            this.positionOther = _position;
+            //this.positionOther = _position;
 
         } //Ende constructor
 
@@ -20,7 +20,7 @@ namespace SoSe21 {
             crc2.strokeStyle = "black";
             crc2.fillStyle = "#808080";
             crc2.lineWidth = 2;
-            crc2.arc(this.positionOther.x, this.positionOther.y, 7, 0, 2 * Math.PI);
+            crc2.arc(this.position.x, this.position.y, 7, 0, 2 * Math.PI);
             crc2.stroke(); 
             crc2.fill();
             crc2.restore();
