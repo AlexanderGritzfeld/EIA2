c9602a85b7339755441def4a15ae09bf5d890bb0
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:12:47 +0200

    Create Vector.js

---
+++ Abschluss/Code/Vector.js
@@ -0,0 +1,19 @@
+"use strict";
+var SoSe21;
+(function (SoSe21) {
+    class Vector {
+        constructor(_x = 0, _y = 0) {
+            this.set(_x, _y);
+        }
+        set(_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+        //Um irgendeinen Vektor zu "speichern" (z.B. für startPos)
+        copy() {
+            return new Vector(this.x, this.y);
+        }
+    } //end class Vector
+    SoSe21.Vector = Vector;
+})(SoSe21 || (SoSe21 = {}));
+//# sourceMappingURL=Vector.js.map
