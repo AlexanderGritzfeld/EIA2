4036f75cd85e50e589d32119317c0267bccfb2b6
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 14:02:08 +0200

    Update Player.js

--- Abschluss/Code/Player.js
+++ Abschluss/Code/Player.js
@@ -5,7 +5,7 @@ var SoSe21;
         constructor(_position, _velocity, _color, _team) {
             super(_position, _velocity);
             this.task = SoSe21.Task.lookForBall;
-            this.radius = 100;
+            this.viewRadius = 500;
             this.color = _color;
             this.startPos = this.position.copy(); //copy siehe Vector
             this.team = _team;
@@ -17,23 +17,104 @@ var SoSe21;
             return this.distancePlayerBall;
         }
         setDistance() {
+            console.log("set distence");
             let ballPos = SoSe21.ball.ballPos;
-            this.distancePlayerBall = SoSe21.Vector.getDistance;
+            console.log("ballPos: " + SoSe21.ball.ballPos);
+            this.distancePlayerBall = SoSe21.Vector.getDistance(ballPos, this.position);
+            console.log("distence fertig");
         }
         draw() {
-            SoSe21.crc2.save();
-            //crc2.translate(this.startPosX, this.startPosY);
+            //crc2.save();
             //console.info("j ist gerade: " + j);
             SoSe21.crc2.beginPath();
             SoSe21.crc2.strokeStyle = "black";
             SoSe21.crc2.fillStyle = this.color;
             SoSe21.crc2.lineWidth = 2;
-            SoSe21.crc2.arc(this.startPos.x, this.startPos.y, 7, 0, 2 * Math.PI);
+            SoSe21.crc2.arc(this.position.x, this.position.y, 7, 0, 2 * Math.PI);
             SoSe21.crc2.stroke();
             SoSe21.crc2.fill();
-            SoSe21.crc2.restore();
+            //crc2.restore();
             console.log("Ich gehöre zu Team: " + this.team);
         } //end draw
+        drawViewRadius() {
+            SoSe21.crc2.beginPath();
+            SoSe21.crc2.arc(this.position.x, this.position.y, this.viewRadius, 0, 2 * Math.PI);
+            SoSe21.crc2.stroke();
+            SoSe21.crc2.closePath();
+        }
+        update() {
+            this.setDistance();
+            console.log("Los geht's");
+            //jenachdem welche Task gerade ansteht
+            switch (this.task) {
+                case SoSe21.Task.lookForBall:
+                    console.log("Ich bin dran");
+                    if (this.distancePlayerBall < this.viewRadius) { //wenn Ball ich Sichtweite --> geh hin
+                        console.log("distance: " + this.distancePlayerBall);
+                        this.task = SoSe21.Task.walkToBall;
+                        console.log("Ich gehe jetzt hin " + this.task);
+                    }
+                    console.log("Ich sehe");
+                    break;
+                case SoSe21.Task.walkToBall:
+                    console.log("Also jetzt gehe ich hin?");
+                    if (this.distancePlayerBall > this.viewRadius) { //wenn Ball nicht (mehr) in Sichtweite --> geh zurück
+                        this.task = SoSe21.Task.walkToStartPos;
+                    }
+                    else {
+                        if (this.distancePlayerBall < 15) { //wenn in Reichweite zum Ball --> schieß
+                            this.task = SoSe21.Task.shootTheBall;
+                            console.log("Ich möchte jetzt schießen");
+                        }
+                        this.movePlayer(SoSe21.ball.ballPos);
+                        console.log("Ich bewege mich zum Ball");
+                    } //end else walkToBall
+                    break;
+                case SoSe21.Task.shootTheBall:
+                    SoSe21.ball.setBoolean(true); // setzt Schießvorgang in Wege
+                    console.log("Ich schieße");
+                    this.task = SoSe21.Task.walkToStartPos; //sobald geschoßen --> Rückzug
+                    break;
+                case SoSe21.Task.walkToStartPos:
+                    if (this.position == this.startPos) { //sobald wieder an Anfangsposition --> schaut nach Ball
+                        this.task = SoSe21.Task.lookForBall;
+                        console.log("Ich bin bereit");
+                    }
+                    this.movePlayer(this.startPos);
+            } //end switch
+        } //end update
+        movePlayer(_position) {
+            let distance = SoSe21.Vector.getDifference(_position, this.position);
+            if (distance.x == 0 && distance.y < 0) { //Fall: Ball ist nördlich von Spieler
+                this.position.y -= this.velocity;
+            }
+            if (distance.x > 0 && distance.y < 0) { //Fall: Ball ist nord-östlich von Spieler
+                this.position.x += this.velocity;
+                this.position.y -= this.velocity;
+            }
+            if (distance.x > 0 && distance.y == 0) { //Fall: Ball ist östlich von Spieler
+                this.position.x += this.velocity;
+            }
+            if (distance.x > 0 && distance.y > 0) { //Fall: Ball ist süd-östlich von Spieler
+                this.position.x += this.velocity;
+                this.position.y += this.velocity;
+            }
+            if (distance.x == 0 && distance.y > 0) { //Fall: Ball ist südlich von Spieler
+                this.position.y += this.velocity;
+            }
+            if (distance.x < 0 && distance.y > 0) { //Fall: Ball ist süd-westlich von Spieler
+                this.position.x -= this.velocity;
+                this.position.y += this.velocity;
+            }
+            if (distance.x < 0 && distance.y == 0) { //Fall: Ball ist westlich von Spieler
+                this.position.x -= this.velocity;
+            }
+            if (distance.x < 0 && distance.y < 0) { //Fall: Ball ist nord-westlich von Spieler
+                this.position.x -= this.velocity;
+                this.position.y -= this.velocity;
+            }
+            this.draw();
+        } //end movePlayer
     } //end class Player
     SoSe21.Player = Player;
 })(SoSe21 || (SoSe21 = {}));
