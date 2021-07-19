f5f7e56c03f94373915041e79ca386a1213861f9
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:11:40 +0200

    bullshit von ersten Mal verbessert
    Position und Vektor werden nun jeweils in einem seperaten File ausgeführt.
    
    Parameter angepasst
    
    Teams unabhängig voneinander gemacht
    
    Teamzugehörigkeit implementiert

--- Abschluss/Code/Player.ts
+++ Abschluss/Code/Player.ts
@@ -2,27 +2,27 @@ namespace SoSe21 {
 
     export class Player extends Movable {
         
-        /*
-        //Startpositionen Team links
+        
+        /*Startpositionen Team links (otherTeam)
         startPosLeft: Vector[] = [{x: 75, y: 350}, {x: 250, y: 100}, {x: 250, y: 600}, {x: 350, y: 250}, {x: 350, y: 450}, {x: 450, y: 350},
-            {x: 675, y: 250}, {x: 675, y: 450}, {x: 750, y: 350}, {x: 750, y: 150}, {x: 750, y: 550}];
+            {x: 675, y: 250}, {x: 675, y: 450}, {x: 750, y: 350}, {x: 750, y: 150}, {x: 750, y: 550}]; 
 
         //Startpositionen Team rechts
-        startPosRight: Vector[] = [{x: 1025, y: 350}, {x: 850, y: 100}, {x: 850, y: 600}, {x: 750, y: 250}, {x: 750, y: 450}, {x: 650, y: 350},
-            {x: 425, y: 250}, {x: 425, y: 450}, {x: 350, y: 350}, {x: 350, y: 150}, {x: 350, y: 550}];
-        */
+        public startPosRight: Vector[] = [{x: 1025, y: 350}, {x: 850, y: 100}, {x: 850, y: 600}, {x: 750, y: 250}, {x: 750, y: 450}, {x: 650, y: 350},
+            {x: 425, y: 250}, {x: 425, y: 450}, {x: 350, y: 350}, {x: 350, y: 150}, {x: 350, y: 550}];*/
+        
 
-        //Startpositionen Team links
+        /*Startpositionen Team links
         private startPosPlayer: Vector[] = [{x: 75, y: 350}, {x: 250, y: 100}, {x: 250, y: 600}, {x: 350, y: 250}, {x: 350, y: 450}, {x: 450, y: 350},
             {x: 675, y: 250}, {x: 675, y: 450}, {x: 750, y: 350}, {x: 750, y: 150}, {x: 750, y: 550},
 
         //Startpositionen Team rechts
             {x: 1025, y: 350}, {x: 850, y: 100}, {x: 850, y: 600}, {x: 750, y: 250}, {x: 750, y: 450}, {x: 650, y: 350},
-            {x: 425, y: 250}, {x: 425, y: 450}, {x: 350, y: 350}, {x: 350, y: 150}, {x: 350, y: 550}];
+            {x: 425, y: 250}, {x: 425, y: 450}, {x: 350, y: 350}, {x: 350, y: 150}, {x: 350, y: 550}]; */
             
-        private color1: string;
-        private color2: string;
-        //private startPos: Vector;
+        private color: string;
+        private startPos: Vector;
+        private team: string;
        // private task: Task = Task.lookForBall;
         //private radius: number = 100;
         //private distancePlayerBall: number;
@@ -30,20 +30,18 @@ namespace SoSe21 {
 
 
 
-        constructor(_position: Vector, _velocity: Vector, _color1: string, _color2: string) {
+        constructor(_position: Vector, _velocity: number, _color: string, _team: string) {
             
             super(_position, _velocity);
 
-            this.color1 = _color1;
-            this.color2 = _color2;
-            //this.startPos
+            this.color = _color;
+            this.startPos = this.position.copy(); //copy siehe Vector
+            this.team = _team;
 
         } //end constructor
 
         
         draw(): void {
-
-            if (j < 11) {
 
             crc2.save();
             //crc2.translate(this.startPosX, this.startPosY);
@@ -52,33 +50,14 @@ namespace SoSe21 {
 
             crc2.beginPath();
             crc2.strokeStyle = "black";
-            crc2.fillStyle = this.color1;
+            crc2.fillStyle = this.color;
             crc2.lineWidth = 2;
-            crc2.arc(this.startPosPlayer[j].x, this.startPosPlayer[j].y, 7, 0, 2 * Math.PI);
+            crc2.arc(this.startPos.x, this.startPos.y, 7, 0, 2 * Math.PI);
             crc2.stroke(); 
             crc2.fill();
             crc2.restore();
 
-            }
-
-            if (j >= 11) {
-
-            //Test
-            crc2.save();
-            //crc2.translate(this.startPosX, this.startPosY);
-
-            crc2.beginPath();
-            crc2.strokeStyle = "black";
-            crc2.fillStyle = this.color2;
-            crc2.lineWidth = 2;
-            crc2.arc(this.startPosPlayer[j].x, this.startPosPlayer[j].y, 7, 0, 2 * Math.PI);
-            crc2.stroke(); 
-            crc2.fill();
-            crc2.restore();
-
-            //console.log("Nach 1 Durchlauf draw(): " + j);
-
-            }
+            console.log("Ich gehöre zu Team: " + this.team);
 
         } //end draw
 
