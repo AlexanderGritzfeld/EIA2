98cc407f54fd8ff2762ad8ac5ae44d02269e6aa0
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:09:30 +0200

    Update Player.js

--- Abschluss/Code/Player.js
+++ Abschluss/Code/Player.js
@@ -2,59 +2,28 @@
 var SoSe21;
 (function (SoSe21) {
     class Player extends SoSe21.Movable {
-        //private startPos: Vector;
         // private task: Task = Task.lookForBall;
         //private radius: number = 100;
         //private distancePlayerBall: number;
-        constructor(_position, _velocity, _color1, _color2) {
+        constructor(_position, _velocity, _color, _team) {
             super(_position, _velocity);
-            /*
-            //Startpositionen Team links
-            startPosLeft: Vector[] = [{x: 75, y: 350}, {x: 250, y: 100}, {x: 250, y: 600}, {x: 350, y: 250}, {x: 350, y: 450}, {x: 450, y: 350},
-                {x: 675, y: 250}, {x: 675, y: 450}, {x: 750, y: 350}, {x: 750, y: 150}, {x: 750, y: 550}];
-    
-            //Startpositionen Team rechts
-            startPosRight: Vector[] = [{x: 1025, y: 350}, {x: 850, y: 100}, {x: 850, y: 600}, {x: 750, y: 250}, {x: 750, y: 450}, {x: 650, y: 350},
-                {x: 425, y: 250}, {x: 425, y: 450}, {x: 350, y: 350}, {x: 350, y: 150}, {x: 350, y: 550}];
-            */
-            //Startpositionen Team links
-            this.startPosPlayer = [{ x: 75, y: 350 }, { x: 250, y: 100 }, { x: 250, y: 600 }, { x: 350, y: 250 }, { x: 350, y: 450 }, { x: 450, y: 350 },
-                { x: 675, y: 250 }, { x: 675, y: 450 }, { x: 750, y: 350 }, { x: 750, y: 150 }, { x: 750, y: 550 },
-                //Startpositionen Team rechts
-                { x: 1025, y: 350 }, { x: 850, y: 100 }, { x: 850, y: 600 }, { x: 750, y: 250 }, { x: 750, y: 450 }, { x: 650, y: 350 },
-                { x: 425, y: 250 }, { x: 425, y: 450 }, { x: 350, y: 350 }, { x: 350, y: 150 }, { x: 350, y: 550 }];
-            this.color1 = _color1;
-            this.color2 = _color2;
-            //this.startPos
+            this.color = _color;
+            this.startPos = this.position.copy(); //copy siehe Vector
+            this.team = _team;
         } //end constructor
         draw() {
-            if (SoSe21.j < 11) {
-                SoSe21.crc2.save();
-                //crc2.translate(this.startPosX, this.startPosY);
-                //console.info("j ist gerade: " + j);
-                SoSe21.crc2.beginPath();
-                SoSe21.crc2.strokeStyle = "black";
-                SoSe21.crc2.fillStyle = this.color1;
-                SoSe21.crc2.lineWidth = 2;
-                SoSe21.crc2.arc(this.startPosPlayer[SoSe21.j].x, this.startPosPlayer[SoSe21.j].y, 7, 0, 2 * Math.PI);
-                SoSe21.crc2.stroke();
-                SoSe21.crc2.fill();
-                SoSe21.crc2.restore();
-            }
-            if (SoSe21.j >= 11) {
-                //Test
-                SoSe21.crc2.save();
-                //crc2.translate(this.startPosX, this.startPosY);
-                SoSe21.crc2.beginPath();
-                SoSe21.crc2.strokeStyle = "black";
-                SoSe21.crc2.fillStyle = this.color2;
-                SoSe21.crc2.lineWidth = 2;
-                SoSe21.crc2.arc(this.startPosPlayer[SoSe21.j].x, this.startPosPlayer[SoSe21.j].y, 7, 0, 2 * Math.PI);
-                SoSe21.crc2.stroke();
-                SoSe21.crc2.fill();
-                SoSe21.crc2.restore();
-                //console.log("Nach 1 Durchlauf draw(): " + j);
-            }
+            SoSe21.crc2.save();
+            //crc2.translate(this.startPosX, this.startPosY);
+            //console.info("j ist gerade: " + j);
+            SoSe21.crc2.beginPath();
+            SoSe21.crc2.strokeStyle = "black";
+            SoSe21.crc2.fillStyle = this.color;
+            SoSe21.crc2.lineWidth = 2;
+            SoSe21.crc2.arc(this.startPos.x, this.startPos.y, 7, 0, 2 * Math.PI);
+            SoSe21.crc2.stroke();
+            SoSe21.crc2.fill();
+            SoSe21.crc2.restore();
+            console.log("Ich gehöre zu Team: " + this.team);
         } //end draw
     } //end class Player
     SoSe21.Player = Player;
