8758d214b9d606d75dd71da9148eb41473596e19
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-07-19 10:12:23 +0200

    Create Position.js

---
+++ Abschluss/Code/Position.js
@@ -0,0 +1,11 @@
+"use strict";
+var SoSe21;
+(function (SoSe21) {
+    //Startpositionen Team links (otherTeam)
+    SoSe21.startPosLeft = [{ x: 75, y: 350 }, { x: 250, y: 100 }, { x: 250, y: 600 }, { x: 350, y: 250 }, { x: 350, y: 450 },
+        { x: 450, y: 350 }, { x: 675, y: 250 }, { x: 675, y: 450 }, { x: 750, y: 350 }, { x: 750, y: 150 }, { x: 750, y: 550 }];
+    //Startpositionen Team rechts
+    SoSe21.startPosRight = [{ x: 1025, y: 350 }, { x: 850, y: 100 }, { x: 850, y: 600 }, { x: 750, y: 250 }, { x: 750, y: 450 },
+        { x: 650, y: 350 }, { x: 425, y: 250 }, { x: 425, y: 450 }, { x: 350, y: 350 }, { x: 350, y: 150 }, { x: 350, y: 550 }];
+})(SoSe21 || (SoSe21 = {}));
+//# sourceMappingURL=Position.js.map
